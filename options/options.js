22936f636481148da26be25c2f1b370df1134573
199X Music <199xmgmt@gmail.com>
2023-03-17 02:01:25 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "toy-delivered-39425",
-  url: "https://toy-delivered-39425.botics.co",
-  api: "https://toy-delivered-39425.botics.co/api/v1"
-}
+  "name": "toy-delivered-39425",
+  "url": "https://toy-delivered-39425.botics.co",
+  "api": "https://toy-delivered-39425.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
-    copy: "Routes available!"
+    "copy": "Routes available!"
   }
-}
+};
